873f08206fdb740b1d3a5b43c6a15e7eb56326c7
smashco <claimr.twotriangle@gmail.com>
2025-07-10 12:07:54 +0530

    Add server-side territory claiming logic

--- server.js
+++ server.js
@@ -10,51 +10,45 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
-// NEW: A simple in-memory store for players' locations
-// In a real game, this would be more complex and possibly backed by a database
+// In-memory store for players' locations and claimed areas
 const players = {};
+const claimedTerritories = []; // <-- NEW: Array to store all claimed polygons
 
 app.get('/', (req, res) => {
   res.send('Claimr Server is running!');
 });
 
-// This is where all our real-time game logic will go
 io.on('connection', (socket) => {
   console.log(`A user connected with ID: ${socket.id}`);
-
-  // Add the new player to our list
   players[socket.id] = { id: socket.id, location: null };
-  
-  // Welcome message to the new user
-  socket.emit('welcome', `Welcome, you are connected! Your ID is ${socket.id}`);
-  
-  // Send the list of existing players to the new player
-  socket.emit('currentPlayers', players);
-
-  // Announce the new player to all other players
   socket.broadcast.emit('newPlayer', players[socket.id]);
 
-  // Listen for a 'locationUpdate' event from a client
+  // <-- NEW: When a player connects, send them all existing territories
+  socket.emit('existingTerritories', claimedTerritories);
+
   socket.on('locationUpdate', (data) => {
-    // For now, just log the data and broadcast it to everyone else
-    console.log(`Received location update from ${socket.id}:`, data);
-    
-    // Update this player's location in our server-side store
     if (players[socket.id]) {
       players[socket.id].location = data;
     }
-
-    // This broadcasts the location to all OTHER connected clients
     socket.broadcast.emit('playerMoved', { id: socket.id, location: data });
+  });
+
+  // <-- NEW: Listen for a 'claimTerritory' event from a client
+  socket.on('claimTerritory', (trailData) => {
+    console.log(`Received territory claim from ${socket.id}`);
+    const newTerritory = {
+      ownerId: socket.id,
+      polygon: trailData, // The list of coordinates
+    };
+    claimedTerritories.push(newTerritory);
+
+    // Broadcast the newly claimed territory to ALL clients
+    io.emit('newTerritoryClaimed', newTerritory);
   });
 
   socket.on('disconnect', () => {
     console.log(`User with ID: ${socket.id} disconnected`);
-    
-    // Remove the player from our list
     delete players[socket.id];
-    
-    // Broadcast that this player has disconnected
     io.emit('playerLeft', { id: socket.id });
   });
 });
